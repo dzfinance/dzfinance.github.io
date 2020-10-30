741fff97ce25b452e924c42f6225c77920e17169
unknown <zhou.dexin@gmail.com>
2020-10-30 00:52:04 -0400

    Updates

---
+++ precache-manifest.092525854cacf44fda9b303d12242d58.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34b482a1a951628f22a74bc358a3e3e",
+    "url": "/dzfinance.github.io/index.html"
+  },
+  {
+    "revision": "9cb9d321271a9cf2fbcb",
+    "url": "/dzfinance.github.io/static/css/2.c2fdf206.chunk.css"
+  },
+  {
+    "revision": "8df94c5dd723f2a62390",
+    "url": "/dzfinance.github.io/static/css/main.c4764aba.chunk.css"
+  },
+  {
+    "revision": "9cb9d321271a9cf2fbcb",
+    "url": "/dzfinance.github.io/static/js/2.5b5b89f7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/dzfinance.github.io/static/js/2.5b5b89f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df94c5dd723f2a62390",
+    "url": "/dzfinance.github.io/static/js/main.d924631a.chunk.js"
+  },
+  {
+    "revision": "236828871f67201dde22",
+    "url": "/dzfinance.github.io/static/js/runtime-main.619c3a25.js"
+  },
+  {
+    "revision": "bf14a6a67b86b991aeafb450ee87eb6b",
+    "url": "/dzfinance.github.io/static/media/Photo Aug 17, 5 03 08 PM-4.bf14a6a6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dzfinance.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dzfinance.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dzfinance.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dzfinance.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dzfinance.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
